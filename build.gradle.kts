cb4520748883e0a7375398210fb3c707f4ad6c75
mazen.rashed@mawdoo3.com <mrmazenrashed@gmail.com>
2020-12-27 22:31:56 +0200

    fix build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,18 +2,21 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.10"
+    `maven-publish`
 }
 
-group = "me.mazen"
-version = "1.0-SNAPSHOT"
+group = "com.mazenrashed"
+version = "1.0.0"
 
 repositories {
+    mavenLocal()
     mavenCentral()
 }
 
 dependencies {
     testImplementation(kotlin("test-junit"))
     implementation("com.squareup.moshi:moshi-kotlin:1.11.0")
+        implementation("com.mazenrashed:moshi-kotlin-extensions:1.0.0")
 }
 
 tasks.test {
@@ -23,3 +26,55 @@ tasks.test {
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
+
+
+
+java {
+    withJavadocJar()
+    withSourcesJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "moshi-kotlin-extensions"
+            from(components["java"])
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("Moshi Kotlin Extensions")
+                description.set("Kotlin extensions for Moshi, Make every thing you want with Moshi in just one line.")
+                url.set("https://github.com/mazenrashed/MoshiKotlinExtensions")
+
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("mazenrashed")
+                        name.set("Mazen Rashed")
+                        email.set("mrmazenrashed@gmail.com")
+                    }
+                }
+            }
+        }
+    }
+    repositories {
+        maven {
+            // change URLs to point to your repos, e.g. http://my.org/repo
+            val releasesRepoUrl = uri("$buildDir/repos/releases")
+            val snapshotsRepoUrl = uri("$buildDir/repos/snapshots")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+        }
+    }
+}
+
